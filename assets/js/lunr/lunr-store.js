7e8eb60fa201e6473562fc19b82af6ca6fd53280
beck <bolun365@163.com>
2022-04-04 00:42:54 +0800

    u

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "1",
+        "excerpt":"11   11  ","categories": ["鲜花"],
+        "tags": [],
+        "url": "/%E9%B2%9C%E8%8A%B1/1/",
+        "teaser": "/assets/basic/teaser_default.jpg"
+      }]
